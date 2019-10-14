530460517916cf886820ce71b0f127249de1d420
Isaac Audet <isaac.audet@gmail.com>
2019-10-14 14:14:02 -0700

    Updated

--- rapleaks.py
+++ rapleaks.py
@@ -2,46 +2,50 @@ import glob
 import os
 import time
 import sys
-from mutagen.easyid3 import EasyID3
-from mutagen.id3 import ID3, APIC
 import magic
 import logging
-from yaspin import yaspin
+import msvcrt
 from tkinter import Tk
+from yaspin import yaspin
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from mutagen.id3 import ID3, APIC
+from mutagen.easyid3 import EasyID3
+from pydrive.auth import GoogleAuth
+from yaspin.spinners import Spinners
+from pydrive.drive import GoogleDrive
+from colorama import init, Fore, Style
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from oauth2client import file, client, tools
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import InvalidArgumentException
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-from oauth2client import file, client, tools
+from selenium.webdriver.support import expected_conditions as EC
 
 # Program created to automaticaly download rap leaks songs. The end goal would be to have is scan reddit and open posts, then automatically click and navigate the links to download files.
 
 # Constants
-location = r"C:\Users\isaac\Documents\SoundDL"
-fid = "1-9HbOEJhzVIRQ4wyglgEVkB9-xqHft5C"
-spinner = yaspin()
+dl_location = r"C:\Users\isaac\Documents\SoundDL"   # location for song dL
+fid = "1-9HbOEJhzVIRQ4wyglgEVkB9-xqHft5C"           # "Leak" folder ID
+spinner = yaspin()                                  # Used for loading icon
+init()                                              # allows for ANSI colours in console
 
 # Webdriver
-options = Options()
+options = Options()                                    
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-prefs = {'download.default_directory': "C:\\Users\\isaac\\Documents\\SoundDL"}
+prefs = {'download.default_directory': dl_location} # Sets DL location
 options.add_experimental_option('prefs', prefs)
-driver = webdriver.Chrome(options=options)
-driver.minimize_window()
-wait = WebDriverWait(driver, 4)
+driver = webdriver.Chrome(executable_path=r"C:\Users\isaac\Desktop\SounDL\chromedriver.exe", options=options)
+driver.minimize_window()                        
+wait = WebDriverWait(driver, 4)                     
 
 
-def download_wait(path_to_downloads):
+def download_wait(path_to_downloads):               # checks path to DL if a file ends with .crdownload. Is used to close the webdriver when the program finishes
     seconds = 0
     dl_wait = True
-    while dl_wait and seconds < 20:
+    while dl_wait and seconds < 40:
         time.sleep(1)
         dl_wait = False
         for fname in os.listdir(path_to_downloads):
@@ -51,8 +55,8 @@ def download_wait(path_to_downloads):
     return seconds  # wait to allow dL to finish
 
 
-def sound_dl(link):
-    driver.get('https://scdownloader.io/')
+def sound_dl(link):                                 # uses selenium to open soundcloud downloader website and paste in the soundcloud link for download.
+    driver.get('https://scdownloader.io/')          # Not optimal
 
     box = wait.until(EC.element_to_be_clickable(
         (By.XPATH, "//div[@id='conversionForm']/form/div/input")))  # text box
@@ -68,9 +72,10 @@ def sound_dl(link):
         result = driver.find_elements_by_xpath(
             "//div[@class='searchboxholder']//a")  # dl button
 
-        if(len(result) > 0):
+        if(len(result) > 0):                       # finds list of elements and clicks the first one. 
             result[0].click()
-            download_wait(location)
+            driver.minimize_window()
+            download_wait(dl_location)
             driver.quit()
             return
         else:
@@ -79,42 +84,41 @@ def sound_dl(link):
         raise
 
 
-def pico_dl(link):
+def pico_dl(link):                              # uses selenium to download off of picosong
     driver.get(link)
-    cont = True
     try:
         box = wait.until(EC.element_to_be_clickable(
-            (By.XPATH, "//ul[@id='audio-actions']/li/a")))
+            (By.XPATH, "//ul[@id='audio-actions']/li/a"))) # first DL button
     except TimeoutException:
-        cont = False
         raise
 
-    if cont == True:
-        box.click()
-        dl = wait.until(EC.element_to_be_clickable(
-            (By.XPATH, "//div[@id='content']/div[2]/div/div/h3/a/strong")))
-        dl.click()
-        driver.minimize_window()
-        download_wait(location)
-        driver.quit()
+    box.click()
+    dl = wait.until(EC.element_to_be_clickable(
+        (By.XPATH, "//div[@id='content']/div[2]/div/div/h3/a/strong"))) # second DL button
+    dl.click()
+    driver.minimize_window()
+    download_wait(dl_location)
+    driver.quit()
 
     
 
-def dbr_dl(link):
+def dbr_dl(link):                                       # uses selenium to download from Dbree
     driver.get(link)
     try:
-        driver.implicitly_wait(5)
+        driver.implicitly_wait(5)                       # implicit wait because of 5 second wait timer when accessing website
         box = wait.until(EC.element_to_be_clickable(
             (By.LINK_TEXT, "Download")))
         box.click()
+        driver.minimize_window()
+
     except TimeoutException:
         raise
-    download_wait(location)
+    download_wait(dl_location)
     driver.quit()
     return
 
 
-def drive_up():
+def drive_up():                                 # Google Drive API to upload most recent file found in DL folder found using glob.
     g_login = GoogleAuth()
     g_login.LoadCredentialsFile("mycreds.txt")
 
@@ -143,19 +147,19 @@ def drive_up():
     file_drive.Upload()
 
 
-def album_art(artist):
+def album_art(artist, name):    # sacad is used to find album art by searching the artist title
     os.chdir(r"C:\Users\isaac\Desktop\SounDL")
-    os.system("sacad " + '"' + artist + '"' + ' "' + "" + '" ' + '"' + "600" '" ' + '"' + artist + ".jpg" + '"')
+    os.system("sacad " + '"' + artist + '"' + ' "' + "" + '" ' + '"' + "600" '" ' + '"' + artist + " - " +  name + ".jpg" + '"')
 
 
-def metadata(file_name, artist, title):
+def metadata(file_name, artist, title):     # easyID3 and ID3 are used to write metadata tags and apply album art found using sacad
     audio = EasyID3(file_name)
     audio['artist'] = artist
     audio['title'] = title
     audio.save()
     
     audio = ID3(file_name)
-    with open(artist+".jpg", 'rb') as albumart:
+    with open(artist + " - " + title + ".jpg", 'rb') as albumart:
         audio['APIC'] = APIC(
                           encoding=3,
                           mime='image/jpeg',
@@ -164,28 +168,28 @@ def metadata(file_name, artist, title):
                         )            
     audio.save()
 
-def file_name():
+def file_name():        # finds the most recent file using glob
     list_of_files = glob.glob('C:\\Users\\isaac\\Documents\\SoundDL\\*')
     new = max(list_of_files, key=os.path.getctime)
     return new
 
-def meta_write(name, artist):
-    with yaspin(text="Applying metadata", color="yellow") as spinner:
+def meta_write(name, artist):   #calls metadata() and renmaes mp3
+    with yaspin(text="Applying metadata", color="cyan") as spinner:
         metadata(file_name() , artist, name)
         head, tail = os.path.split(file_name())
-        song_name = head + "\\" + name + ".mp3"
+        song_name = head + "\\" + artist + " - " + name + ".mp3"
         os.rename(file_name(), song_name)
         spinner.ok("✅ ")
 
-    with yaspin(text="Uploading to Google Drive", color="yellow") as spinner:   
+    with yaspin(text="Uploading to Google Drive", color="cyan") as spinner:   
         drive_up()
         spinner.ok("✅ ")
     
 
-def link_finder():
+def link_finder():      # finds links on reddit posts by searching for the DL download links      
     method = "pico"
     reddit = Tk().clipboard_get()
-    with yaspin(text="Loading reddit", color="yellow") as spinner:
+    with yaspin(text="Loading reddit", color="cyan") as spinner:
         try:
             driver.get(reddit)
         except InvalidArgumentException:
@@ -197,15 +201,15 @@ def link_finder():
     title = driver.title
     artist = title.split("]")[1].split(" -")[0]
     name = title.split("]")[1].split(" - ")[1]. split(" :")[0]
-    with yaspin(text="Finding album art", color="yellow") as spinner:
+    with yaspin(text="Finding album art", color="cyan") as spinner:
         try:
-            album_art(artist)
+            album_art(artist, name)
             spinner.ok("✅ ")
         except:
             spinner.fail("❌ ")
 
     if method == "pico":
-        with yaspin(text="Downloading with Picosong", color="yellow") as spinner:
+        with yaspin(text="Downloading with Picosong", color="cyan") as spinner:
             try:
                 pico = wait.until(EC.element_to_be_clickable(
                     (By.PARTIAL_LINK_TEXT, "picosong")))
@@ -216,7 +220,7 @@ def link_finder():
                 return
 
             except TimeoutException:
-                spinner.write("> Find by link text failed")
+                spinner.write(">Find by link text failed")
                 pass
             try:
                 pico = driver.find_element_by_xpath("//*[contains(text(), 'Picosong')]")
@@ -225,39 +229,47 @@ def link_finder():
                 spinner.ok("✅ ")
                 meta_write(name, artist)
                 return 
+            
             except TimeoutException:
                 method = "sound"
                 spinner.fail("❌ ")
-                 
+
 
     if method == "sound":
-        with yaspin(text="Downloading with Soundcloud", color="yellow") as spinner:
+        with yaspin(text="Downloading with Soundcloud", color="cyan") as spinner:
             try:
                 driver.get(reddit)
                 sound = wait.until(EC.element_to_be_clickable(
                     (By.PARTIAL_LINK_TEXT, "soundcloud")))
                 link = sound.get_attribute("href")
-                sound_dl(link)
+                os.chdir(r"C:\Users\isaac\Documents\SoundDL")
+                os.system("scdl -l " + Tk().clipboard_get())
+                os.chdir(r"C:\Users\isaac\Desktop\SounDL")
                 spinner.ok("✅ ")
-                meta_write(name, artist)
-                return
+                with yaspin(text="Uploading to Google Drive", color="cyan") as spinner:   
+                    drive_up()
+                    spinner.ok("✅ ")
             except TimeoutException:
-                spinner.write("> Find by link text failed")
+                spinner.write(">Find by link text failed")
             try:
                 driver.get(reddit)
                 sound = driver.find_element_by_xpath("//*[contains(text(), 'SoundCloud')]")
                 link = sound.get_attribute("href")
-                sound_dl(link)
+                os.chdir(r"C:\Users\isaac\Documents\SoundDL")
+                os.system("scdl -l " + Tk().clipboard_get())
+                os.chdir(r"C:\Users\isaac\Desktop\SounDL")
                 spinner.ok("✅ ")
-                meta_write(name, artist)
-                return
+                with yaspin(text="Uploading to Google Drive", color="cyan") as spinner:   
+                    drive_up()
+                    spinner.ok("✅ ")
+                    return
             except TimeoutException:
                 spinner.fail("❌ ")
                 method = "dbree"
  
 
     if method == "dbree":
-        with yaspin(text="Downloading with Dbree", color="yellow") as spinner:
+        with yaspin(text="Downloading with Dbree", color="cyan") as spinner:
             try:
                 driver.get(reddit)
                 sound = wait.until(EC.element_to_be_clickable(
@@ -268,7 +280,7 @@ def link_finder():
                 meta_write(name, artist)
                 return
             except TimeoutException:
-                spinner.write("> Find by link text failed")
+                spinner.write(">Find by link text failed")
             try:
                 driver.get(reddit)
                 dbr = driver.find_element_by_xpath("//*[contains(text(), 'Dbree')]")
@@ -278,10 +290,82 @@ def link_finder():
                 meta_write(name, artist)
             except TimeoutException:
                 spinner.fail("❌ ")
-                spinner.write("> Failed to find any links")
+                spinner.write(">Failed to find any links")
                 driver.quit()
 
+def user_input():       # UI to facilitate using whochever download means necesssary
+    print("[1]: " + Fore.MAGENTA + Style.BRIGHT + "Reddit" + Style.RESET_ALL + " Download")
+    print("[2]: " + Fore.YELLOW + Style.DIM + "Soundcloud" + Style.RESET_ALL  + " Download")
+    print("[3]: " + Fore.GREEN + Style.DIM + "Picosong" + Style.RESET_ALL + " Download")
+    print("[4]: " + Fore.BLUE + "Dbree" + Fore.RESET +  " Download")
+    print("\n[5]: " + Fore.RED + "Exit" + Fore.RESET)
+    option = msvcrt.getche()
+    os.system("cls")
+    while option != b"5":
+        if option == b"1":
+            option = "5"
+            link_finder()
+        
+        elif option == b"2":
+            option = b"5"
+            with yaspin(text="Downloading with Soundcloud", color="cyan") as spinner:
+                driver.quit()
+                os.chdir(r"C:\Users\isaac\Documents\SoundDL")
+                os.system("scdl -l " + Tk().clipboard_get())
+                os.chdir(r"C:\Users\isaac\Desktop\SounDL")
+                spinner.ok("✅ ")
+                with yaspin(text="Uploading to Google Drive", color="cyan") as spinner:   
+                    drive_up()
+                    spinner.ok("✅ ")
+                
+                
+        elif option == b"3":
+            option = b"5"
+            with yaspin(text="Downloading with Picosong", color="cyan") as spinner:
+                try:
+                    driver.get(Tk().clipboard_get())
+                    name = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@id='song-info']/table/tbody/tr/td[2]"))).text
+                    artist = driver.find_element_by_xpath("//div[@id='song-info']/table/tbody/tr[2]/td[2]").text
+                    pico_dl(Tk().clipboard_get())
+                    spinner.ok("✅ ")
+                    with yaspin(text="Finding album art", color="cyan") as spinner:
+                        album_art(artist, name)
+                        spinner.ok("✅ ")
 
+                    meta_write(name, artist)
+                    spinner.write(artist + " - " + name + " downloaded.")   
+                except (TimeoutException, InvalidArgumentException) as n:
+                    spinner.fail("❌ ")
+                    driver.quit()
+                    return print("Exception" + str(n) + "thrown.")
+        
+        elif option == b"4":
+            option = b"5"
+            with yaspin(text="Downloading with Dbree", color="cyan") as spinner:
+                try:
+                    dbr_dl(Tk().clipboard_get())
+                    spinner.ok("✅ ")
+                    with yaspin(text="Finding album art", color="cyan") as spinner:
+                        album_art(artist, name)
+                        spinner.ok("✅ ")
+
+                        meta_write(name, artist)
+                        spinner.write(artist + " - " + name + " downloaded.")   
+                    with yaspin(text="Uploading to Google Drive", color="cyan") as spinner:   
+                        drive_up()
+                        spinner.ok("✅ ")
+                except (TimeoutException, InvalidArgumentException) as n:
+                    spinner.fail("❌ ")
+                    driver.quit()
+                    return print("Exception" + str(n) + "thrown.")
+
+        elif option == b"5":
+            driver.quit()
+            return
+        
+        else:
+            driver.quit()
+            option = msvcrt.getche("Undefined input, try again!")
 
 if __name__ == "__main__":
-    link_finder()
+    user_input()
